aeaacffd1b95a7c82b3a238e150adbd387dccc51
aliouba <alioubafr@gmail.com>
2015-04-28 08:01:09 +0200

    Add minimum of plants and check if user is authed

--- presta_viticoles/urls.py
+++ presta_viticoles/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from presta_viticoles.views import *
+from presta_viticoles.viewsapi import *
 from rest_framework import routers
 
 
@@ -12,7 +13,8 @@ urlpatterns = patterns('',
     url(r'^api/Cbenefits/(?P<customerID>[0-9]+)/$',EstimatesCustomerList.as_view()),
     url(r'^api/CbenefitsList/(?P<customerID>[0-9]+)/$',CEstimatesList.as_view()),
     url(r'^make_estimate/(?P<siret>[0-9]+)/$', make_estimate),
-    url(r'^Clogin$',login_customer, name='Clogin'),
-    url(r'^Cbenefits/(?P<customerID>[0-9]+)/$',CEstimatesView.as_view(), name='estimates_for_customer'),
+    url(r'^Clogin/$',login_customer, name='Clogin'),
+    url(r'^Clogout/$',logout_customer, name='Clogout'),
+    url(r'^Cbenefits/(?P<customerID>[0-9]+)/$',estimates_customer, name='estimates_for_customer'),
     #url(r'^Ebenefits/(?P<siret>[0-9]+)/$',Ebenefits, name='benefits_for_companies'),
 )
